978710c880948e00e73edc81b37af6571e7a38b8
Truongman <txtruong2005@gmail.com>
2026-06-20 14:47:37 +0700

    Remove redundant comment

--- src/hooks/useSubmit.jsx
+++ src/hooks/useSubmit.jsx
@@ -4,10 +4,6 @@ import {emailJSConfig} from "../config/siteConfig";
 
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-/**
- * This is a custom hook that can be used to submit a form and simulate an API call
- * It uses Math.random() to simulate a random success or failure, with 50% chance of each
- */
 const useSubmit = () => {
   const [isLoading, setLoading] = useState(false);
   const [response, setResponse] = useState(null);
